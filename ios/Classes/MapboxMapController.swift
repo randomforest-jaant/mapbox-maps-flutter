7f1f3c7ce27541b2967129a4f53680b125846454
Roman Laitarenko <roman.laitarenko@mapbox.com>
2024-01-29 14:03:26 +0200

    [Debug] remove channels cleanup

--- ios/Classes/MapboxMapController.swift
+++ ios/Classes/MapboxMapController.swift
@@ -195,18 +195,18 @@ class MapboxMapController: NSObject, FlutterPlatformView {
 
     private func releaseMethodChannels() {
 //        channel.setMethodCallHandler(nil)
-        SetUpFLTStyleManager(proxyBinaryMessenger, nil)
-        SetUpFLT_CameraManager(proxyBinaryMessenger, nil)
-        SetUpFLT_MapInterface(proxyBinaryMessenger, nil)
-        SetUpFLTProjection(proxyBinaryMessenger, nil)
-        SetUpFLT_AnimationManager(proxyBinaryMessenger, nil)
-        SetUpFLT_SETTINGS_LocationComponentSettingsInterface(proxyBinaryMessenger, nil)
-        SetUpFLT_SETTINGSGesturesSettingsInterface(proxyBinaryMessenger, nil)
-        SetUpFLT_SETTINGSLogoSettingsInterface(proxyBinaryMessenger, nil)
-        SetUpFLT_SETTINGSAttributionSettingsInterface(proxyBinaryMessenger, nil)
-        SetUpFLT_SETTINGSCompassSettingsInterface(proxyBinaryMessenger, nil)
-        SetUpFLT_SETTINGSScaleBarSettingsInterface(proxyBinaryMessenger, nil)
-        annotationController?.tearDown(messenger: proxyBinaryMessenger)
+//        SetUpFLTStyleManager(proxyBinaryMessenger, nil)
+//        SetUpFLT_CameraManager(proxyBinaryMessenger, nil)
+//        SetUpFLT_MapInterface(proxyBinaryMessenger, nil)
+//        SetUpFLTProjection(proxyBinaryMessenger, nil)
+//        SetUpFLT_AnimationManager(proxyBinaryMessenger, nil)
+//        SetUpFLT_SETTINGS_LocationComponentSettingsInterface(proxyBinaryMessenger, nil)
+//        SetUpFLT_SETTINGSGesturesSettingsInterface(proxyBinaryMessenger, nil)
+//        SetUpFLT_SETTINGSLogoSettingsInterface(proxyBinaryMessenger, nil)
+//        SetUpFLT_SETTINGSAttributionSettingsInterface(proxyBinaryMessenger, nil)
+//        SetUpFLT_SETTINGSCompassSettingsInterface(proxyBinaryMessenger, nil)
+//        SetUpFLT_SETTINGSScaleBarSettingsInterface(proxyBinaryMessenger, nil)
+//        annotationController?.tearDown(messenger: proxyBinaryMessenger)
     }
 
     final class HttpUseragentInterceptor: HttpServiceInterceptorInterface {
